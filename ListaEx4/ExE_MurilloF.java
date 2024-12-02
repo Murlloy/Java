043f3765ce415716e06ce79e2f6f8d52f68e71dd
Murlloy <minguerilogenshin@gmail.com>
2024-12-02 15:58:36 -0300

    feat:mudei :>

--- ListaEx4/ExE_MurilloF.java
+++ ListaEx4/ExE_MurilloF.java
@@ -3,9 +3,9 @@ package ListaEx4;
 import java.util.Scanner;
 
 public class ExE_MurilloF {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner ler = new Scanner(System.in);
 
         int fatorial = 1;
@@ -13,7 +13,7 @@ public class ExE_MurilloF {
         int[] mA = new int[15];
         int[] mB = new int[15];
 
-        for(int i=0;i<15;){
+        for (int i = 0; i < 15;) {
 
             try {
 
@@ -22,16 +22,16 @@ public class ExE_MurilloF {
 
                 fatorial = 1;
 
-                for(int l = 1; l <= mA[i]; l++){
+                for (int l = 1; l <= mA[i]; l++) {
                     fatorial = fatorial * l;
                 }
 
                 mB[i] = fatorial;
 
                 i++;
-                
+
             } catch (Exception e) {
-               
+
                 System.out.println("Digite um valor valido por favor!!!");
                 ler.next();
 
@@ -40,8 +40,8 @@ public class ExE_MurilloF {
 
         ler.close();
 
-        for(int i = -1; i < 15; i++){
-            if(i == -1)
+        for (int i = -1; i < 15; i++) {
+            if (i == -1)
                 System.out.println("Matriz B: ");
             else
                 System.out.println(i + ": " + mB[i]);
